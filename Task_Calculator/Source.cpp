427817fe15892c0b64ae81ebeb1f8b7cb4d79a4c
MadMks <mks59k@gmail.com>
2018-05-31 20:56:30 +0300

    Визуальное оформление.

---
+++ Task_Calculator/Source.cpp
@@ -0,0 +1,46 @@
+#include <windows.h>
+#include <tchar.h>
+#include "resource.h"
+#include <vector>
+using namespace std;
+
+
+BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR lpszCmdLine, int nCmdShow)
+{
+
+	MSG msg;
+	HWND hDialog = CreateDialog(hInst, MAKEINTRESOURCE(IDD_DIALOG_Main), NULL, DlgProc);
+	ShowWindow(hDialog, nCmdShow);
+
+
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+
+
+BOOL CALLBACK DlgProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam)
+{
+
+	switch (uMessage)
+	{
+	case WM_CLOSE:
+		DestroyWindow(hWnd);
+		PostQuitMessage(0);
+		return TRUE;
+
+	case WM_INITDIALOG:
+
+
+		return TRUE;
+
+
+	}
+	return FALSE;
+}
